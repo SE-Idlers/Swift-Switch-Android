e48bfadb35f6a87ab385d27501c441c2a94648a9
Capus <40078026+Capus@users.noreply.github.com>
2019-04-14 22:17:16 +0800

    Delete Discriminator.java

--- app/src/main/java/com/example/win/easy/recognization/recognization/Discriminator.java
+++
@@ -1,9 +0,0 @@
-package com.example.win.easy.recognization.recognization;
-
-import java.util.HashMap;
-
-public interface Discriminator {
-
-    Character discriminate(HashMap<Character, Float> Probability);
-
-}
