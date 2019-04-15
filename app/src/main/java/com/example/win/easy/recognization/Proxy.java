ec45294d799e53cc0b2340bdd98ef27f753bad21
Capus <40078026+Capus@users.noreply.github.com>
2019-04-15 12:00:36 +0800

    在Proxy中添加Image

--- app/src/main/java/com/example/win/easy/recognization/Proxy.java
+++ app/src/main/java/com/example/win/easy/recognization/Proxy.java
@@ -1,6 +1,8 @@
 package com.example.win.easy.recognization;
 
 
+import android.gesture.Gesture;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -12,13 +14,21 @@ public class Proxy implements RecognizationProxy {
     private static RecognizationAdapter myAdapter = new Adapter();
     private static Discriminator myDecisionMaker = new DecisionMaker();
     private static int CurrentId = 0;
+    private static RecognizationUnit myImage;
+
+    public boolean loadGesture(Gesture gesture, int positionId){
+        //下列语句可更改
+        myImage = new PositionedImage();
+        ((PositionedImage) myImage).create(gesture, positionId);
+        return (((PositionedImage) myImage).getBitmap()!=null);
+    }
 
     public List<Character> receive(RecognizationUnit unit){
         //需要在能调用getAssets()的地方加上下列语句
         //adapter.inferenceInterface = new TensorFlowInferenceInterface(getAssets(),adapter.model_file);
-        myMap = myAdapter.recognize(((PositionedImage)unit).getFloat_array());
-        Character character = myDecisionMaker.discriminate(myMap);
         if(unit instanceof PositionedImage){
+            myMap = myAdapter.recognize(((PositionedImage)unit).getFloat_array());
+            Character character = myDecisionMaker.discriminate(myMap);
             int index = ((PositionedImage)unit).getGestureId();
             if(index != CurrentId){
                 myList.add(character);
@@ -29,6 +39,8 @@ public class Proxy implements RecognizationProxy {
             CurrentId = index;//更新Id
         }
         if(unit instanceof Image){
+            myMap = myAdapter.recognize(((Image)unit).getFloat_array());
+            Character character = myDecisionMaker.discriminate(myMap);
             myList.add(character);
         }
         return myList;
