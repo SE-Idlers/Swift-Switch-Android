d15871c4f07a4353fa71862214ac99bb483d8424
Guo <iyilan@qq.com>
2019-05-12 22:25:53 +0800

    rebuild view code architecture

--- app/src/main/java/com/example/win/easy/listener/OnClickListenerForSelectingSong.java
+++ app/src/main/java/com/example/win/easy/listener/OnClickListenerForSelectingSong.java
@@ -2,21 +2,20 @@ package com.example.win.easy.listener;
 
 import android.view.View;
 
+import com.example.win.easy.activity.LockActivity;
 import com.example.win.easy.display.DisplayManagerImpl;
 import com.example.win.easy.display.interfaces.DisplayManager;
 import com.example.win.easy.song.Song;
 import com.example.win.easy.songList.SongListMangerImpl;
 import com.example.win.easy.songList.interfaces.SongListManager;
-import com.example.win.easy.view.DashboardView;
-import com.example.win.easy.view.MediaPlayerView;
 import com.example.win.easy.view.interfaces.SongListView;
 
 public class OnClickListenerForSelectingSong implements View.OnClickListener {
 
     private SongListManager songListManager=SongListMangerImpl.getInstance();
-    private SongListView songListView=DashboardView.getInstance();
+    private SongListView songListView=LockActivity.lockActivity;
     private DisplayManager displayManager=DisplayManagerImpl.getInstance();
-    private MediaPlayerView mediaPlayerView=MediaPlayerView.getInstance();
+    private LockActivity lockActivity=LockActivity.lockActivity;
 
     private Song song;
     public OnClickListenerForSelectingSong(Song song){
@@ -27,6 +26,6 @@ public class OnClickListenerForSelectingSong implements View.OnClickListener {
     public void onClick(View v) {
         songListView.update(song);
         displayManager.restartWith(song,songListManager.appearanceListsOf(song).get(0));
-        mediaPlayerView.updatePauseView();
+        lockActivity.updatePauseView();
     }
 }
